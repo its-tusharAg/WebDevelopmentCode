0f3c0e64a4973b427705699a81f21e47184f0d6e
its-tusharAg <officialtjtushar@gmail.com>
2024-01-10 21:43:47 +0530

    trying to creating ebsite

--- Aapna Collage/Delta/11-20/day 16/arr.js
+++ Aapna Collage/Delta/11-20/day 16/arr.js
@@ -8,8 +8,24 @@ console.log(arr1[3]); // out of range
 console.log(typeof arr1);
 arr1[10] = "tushar"
 console.log(arr1);
-let arr2 = ["tushar", "anshika", "shivani"];
+let arr2 = ["tushar", "aman", "rohan", "anshika", "shivani"];
 console.log(arr1.concat(arr2));
 console.log(arr1.includes("halwa"));
-console.log(arr1.reverse())
-console.log(arr2.slice(0, 1))
+console.log(arr1.reverse());
+console.log(arr2.slice(0, 1));
+// splice
+console.log(arr2.splice(3));
+console.log(arr2);
+console.log(arr2.splice(1, 2, "Halwa", "secondHala"));
+console.log(arr2);
+console.log(arr2.sort());
+console.log(arr2);
+
+// Lec question
+// q1
+let monthArr = ["january", "july", "march", "august"];
+monthArr.splice(0, 2, "july", "june");
+console.log(monthArr);
+// q2
+let langArr = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+console.log(langArr.reverse().indexOf("javascript"));
