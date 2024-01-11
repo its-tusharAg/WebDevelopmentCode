220120190b36214f8b28a6217c1fd31429ebc688
its-tusharAg <officialtjtushar@gmail.com>
2024-01-11 23:44:02 +0530

    completed side bar of my portfolio

---
+++ Aapna Collage/Delta/11-20/day 17/app.js
@@ -0,0 +1,30 @@
+for(let i=0; i<5; i++){
+    console.log(i);
+}
+
+// prinitng odd nums
+for(let i=1; i<=20; i++){
+    if(i%2 != 0){
+        console.log(i);
+    }
+}
+
+// even nums
+for(let i=1; i<=15; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+// table of 5
+// let num = prompt("Enter the number:");
+// for(let i=1; i<=10; i++){
+//     console.log(i*num);
+// }
+
+// while loop
+let i = 0;
+while(i<5){
+    console.log(i);
+    i++;
+}
--- Project/Portfolio/js/app.js
+++ Project/Portfolio/js/app.js
@@ -1,8 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider').slick({
         arrows: false,
         dots: true,
         appendDots: '.slider-dots',
         dotsClass: 'dots'
     });
+
+
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+
+
+
+    times.addEventListener('click', function () {
+        mobileNav.classList.remove('open');
+    })
+
+    hamberger.addEventListener('click', function () {
+        mobileNav.classList.add('open');
+    })
 });
+
