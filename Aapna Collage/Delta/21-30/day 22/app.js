79a7f2490a0639aae2dd142e01ea7771fc8bace1
its-tusharAg <officialtjtushar@gmail.com>
2024-01-22 16:00:30 +0530

    doing day 21, in this i learning about dom

--- Aapna Collage/Delta/21-30/day 22/app.js
+++ Aapna Collage/Delta/21-30/day 22/app.js
@@ -2,3 +2,9 @@ console.log("Hello world");
 console.log(window);
 console.log(document);
 console.dir(document);
+console.dir(document.all);
+document.all[8].innerText = "Tushar"; 
+
+let mainImg = document.getElementById("main-img");
+console.dir(mainImg);
+mainImg.src = "https://upload.wikimedia.org/wikipedia/en/b/bd/Issue252.jpg";
