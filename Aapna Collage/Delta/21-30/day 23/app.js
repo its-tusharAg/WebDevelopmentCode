dc03357d2a9295c2151746d633228667ba03192b
its-tusharAg <officialtjtushar@gmail.com>
2024-01-25 16:28:43 +0530

    adding js data

---
+++ Aapna Collage/Delta/21-30/day 23/app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button");
+// btn.onclick = function () {
+//     console.log("it clicked");
+// };
+btn.onclick = () => {
+    alert("it clicked")
+     // console.log("it clicked");
+};
