a536a13ce806ebb7c23afe0555123147a62708bc
its-tusharAg <officialtjtushar@gmail.com>
2024-02-13 19:21:08 +0530

    adding node js code

--- Aapna Collage/Delta/21-30/day 30/app.js
+++ Aapna Collage/Delta/21-30/day 30/app.js
@@ -1,3 +1,20 @@
-for(let i=0; i<5; i++){
-    console.log(i);
+// for(let i=0; i<5; i++){
+//     console.log(i);
+// }
+
+// let args = process.argv;
+// for(let i=2; i<args.length; i++){
+//     console.log("welcome " + args[i]);
+// }
+
+// importing code/method from another file within same directory
+const math = require("./math");
+console.log(math.mult(2,4));
+console.log(math.g);
+
+// importing code/methods form another file form diff directory
+const fruitInfo = require("./Fruits/index");
+console.log(fruitInfo);
+for(let i=0; i<fruitInfo.length; i++){
+    console.log(fruitInfo[i].name);
 }
---
+++ Aapna Collage/Delta/21-30/day 30/math.js
@@ -0,0 +1,12 @@
+const sum = (a, b)=> a+b;
+const mult = (a, b)=> a*b;
+const g = 9.8;
+const PI = 3.14;
+
+const obj = {
+    sum: sum,
+    mult: mult,
+    g: g,
+    PI: PI
+};
+module.exports = obj;
