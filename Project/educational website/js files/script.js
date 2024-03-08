ff1880144542a9e273c7af1a3cce2aabe690d64e
its-tusharAg <officialtjtushar@gmail.com>
2024-03-08 19:37:40 +0530

    updating edu website code

--- Project/educational website/js files/script.js
+++ Project/educational website/js files/script.js
@@ -17,21 +17,21 @@ $(document).ready(function () {
   });
 });
 
-
 // code to change conntact us page
 let stBtn = document.querySelector(".st-btn");
 let trBtn = document.querySelector(".tr-btn");
 let trinerPg = document.querySelector(".trainer-page");
 let studentPg = document.querySelector(".stud-page");
 
-
-
 trBtn.addEventListener("click", () => {
   trinerPg.classList.remove("hide");
   studentPg.classList.add("hide");
-})
+});
 stBtn.addEventListener("click", () => {
   trinerPg.classList.add("hide");
   studentPg.classList.remove("hide");
-})
+});
+// infinite slider
 
+let copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
