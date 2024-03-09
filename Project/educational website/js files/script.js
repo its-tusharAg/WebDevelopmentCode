dfc42b3b56aa8ae0b905e04019f3ebc0ff83b69e
its-tusharAg <officialtjtushar@gmail.com>
2024-03-10 03:14:52 +0530

    completed form styling and also fix the js code

--- Project/educational website/js files/script.js
+++ Project/educational website/js files/script.js
@@ -1,37 +1,52 @@
-$(document).ready(function () {
-  $(".slider").slick({
-    arrows: false,
-    dots: true,
-    appendDots: ".slider-dots",
-    dotsClass: "dots",
-  });
+// $(document).ready(function () {
+//   $(".slider").slick({
+//     arrows: false,
+//     dots: true,
+//     appendDots: ".slider-dots",
+//     dotsClass: "dots",
+//   });
 
-  let hamburger = document.querySelector(".hamburger");
-  let times = document.querySelector(".times");
-  let mobileNav = document.querySelector(".mobile-nav");
-  hamburger.addEventListener("click", function () {
-    mobileNav.classList.add("open");
-  });
-  times.addEventListener("click", function () {
-    mobileNav.classList.remove("open");
-  });
-});
+//   let hamburger = document.querySelector(".hamburger");
+//   let times = document.querySelector(".times");
+//   let mobileNav = document.querySelector(".mobile-nav");
+//   hamburger.addEventListener("click", function () {
+//     mobileNav.classList.add("open");
+//   });
+//   times.addEventListener("click", function () {
+//     mobileNav.classList.remove("open");
+//   });
+// });
 
 // code to change conntact us page
-let stBtn = document.querySelector(".st-btn");
-let trBtn = document.querySelector(".tr-btn");
-let trinerPg = document.querySelector(".trainer-page");
-let studentPg = document.querySelector(".stud-page");
+let trBtn = document.querySelector("#train-btn");
+let stBtn = document.querySelector("#stud-btn");
+let stdForm = document.querySelector("#std-form");
+let trForm = document.querySelector("#trai-form");
 
 trBtn.addEventListener("click", () => {
-  trinerPg.classList.remove("hide");
-  studentPg.classList.add("hide");
+  trForm.classList.remove("hide");
+  stdForm.classList.add("hide");
 });
 stBtn.addEventListener("click", () => {
-  trinerPg.classList.add("hide");
-  studentPg.classList.remove("hide");
+  trForm.classList.add("hide");
+  stdForm.classList.remove("hide");
 });
-// infinite slider
+// // infinite slider
 
-let copy = document.querySelector(".logos-slide").cloneNode(true);
-document.querySelector(".logos").appendChild(copy);
+// let copy = document.querySelector(".logos-slide").cloneNode(true);
+// document.querySelector(".logos").appendChild(copy);
+
+// js for form
+// let trBtn = document.querySelector("#train-btn");
+// let strBtn = document.querySelector("#stud-btn");
+// let stdForm = document.querySelector("#std-form");
+// let trForm = document.querySelector("#trai-form");
+
+// trBtn.addEventListener("click", function() {
+//     trForm.classList.remove("hide");
+//     console.log("halwa");
+// })
+// strBtn.addEventListener("click", function() {
+//     // trForm.classList.remove("hide");
+//     console.log("halwa");
+// })
