f6092b4a7482b8be2c41ae147e07abfb284480fc
its-tusharAg <officialtjtushar@gmail.com>
2024-03-20 00:17:36 +0530

    adding data

---
+++ Learning/code step by step/Node js/Day 1-10/index.js
@@ -0,0 +1,10 @@
+// console.log("Halwa hia dosto");
+
+// day 7
+const http = require("http");
+
+function datacenter(req, res) {
+    res.write("<h1>Hollaa thats my server</h1>")
+    res.end();
+}
+http.createServer(datacenter).listen(8080);
