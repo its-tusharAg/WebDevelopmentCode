0d3ff98e7580be435de50824a4356c2ebb8cf7d0
its-tusharAg <officialtjtushar@gmail.com>
2024-03-21 00:13:59 +0530

    adding todo list js code

--- Learning/code step by step/Node js/Day 1-10/index.js
+++ Learning/code step by step/Node js/Day 1-10/index.js
@@ -3,12 +3,10 @@
 // day 7
 const http = require("http");
 // const colors = require("colors");
-// const chalk = require("chalk");
-import {chalk} from "chalk";
-console.log(chalk.blue("halwa"));
-// function datacenter(req, res) {
-//     res.write("<h1>Hollaa thats my server</h1>")
-//     res.end();
-// }
 
-// http.createServer(datacenter).listen(8080);
+function datacenter(req, res) {
+    res.write("<h1>Hollaa thats my server</h1>")
+    res.end();
+}
+
+http.createServer(datacenter).listen(8080);
