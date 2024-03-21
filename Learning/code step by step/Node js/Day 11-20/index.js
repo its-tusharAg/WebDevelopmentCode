061dbfe7ae437b76aa296a2e3fbe3b4882b14ec6
its-tusharAg <officialtjtushar@gmail.com>
2024-03-22 00:07:17 +0530

    addingn node js code

---
+++ Learning/code step by step/Node js/Day 11-20/index.js
@@ -0,0 +1,28 @@
+// const http = require("http");
+// let data = require("./data");
+
+// const server = http.createServer((req, res) =>{
+//     res.writeHead(200, {"Content-Type":"application/json"});
+//     // res.write(JSON.stringify({
+//         // name:"Tushar",
+//         // Email:"tjtushar7830@gmail.com"
+//     // }));
+//     res.write(JSON.stringify(data));
+//     // res.write("Halwa hai dosto");
+//     res.end();
+// });
+
+// server.listen(8000);
+
+// creation of files
+
+const fs = require("fs");
+
+const input = process.argv;
+console.log(input);
+if(input[2] == "add"){
+    fs.writeFileSync(input[3], "halwa hai dsoto");
+}
+else if(input[2] == "del"){
+    fs.unlinkSync(input[3])
+}
