8e81bc3daea5643ad48d5fd87d1985af3711190c
its-tusharAg <officialtjtushar@gmail.com>
2024-03-23 21:00:29 +0530

    adding

--- Learning/code step by step/Node js/Day 11-20/index.js
+++ Learning/code step by step/Node js/Day 11-20/index.js
@@ -1,18 +1,18 @@
 const http = require("http");
 let data = require("./data");
 
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, {"Content-Type":"application/json"});
-    // res.write(JSON.stringify({
-        // name:"Tushar",
-        // Email:"tjtushar7830@gmail.com"
-    // }));
-    res.write(JSON.stringify(data));
-    // res.write("Halwa hai dosto");
-    res.end();
-});
+// const server = http.createServer((req, res) =>{
+//     res.writeHead(200, {"Content-Type":"application/json"});
+//     // res.write(JSON.stringify({
+//         // name:"Tushar",
+//         // Email:"tjtushar7830@gmail.com"
+//     // }));
+//     res.write(JSON.stringify(data));
+//     // res.write("Halwa hai dosto");
+//     res.end();
+// });
 
-server.listen(8000);
+// server.listen(8000);
 
 // creation of files
 
@@ -29,3 +29,20 @@ server.listen(8000);
 // else{
 //     console.log("invalid input");
 // }
+
+let fs = require("fs");
+let path = require("path");
+
+let dirPath = path.join(__dirname, "files")
+// console.log(dirPath);
+// for(let i=0; i<5; i++){
+//     fs.writeFileSync(`${dirPath}/doc${i}.txt`, "this is a file");
+//    console.log(fs.readFileSync(`${dirPath}/doc${i}.txt`)); 
+// }
+// let i = 0;
+
+// to get/print file name
+
+fs.readdir(dirPath, (err, file) =>{
+    console.log(file);
+})
