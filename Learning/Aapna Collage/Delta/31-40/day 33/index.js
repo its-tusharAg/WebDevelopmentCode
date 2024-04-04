3081f2d65ec422e187a7274daa19ef43ec7fcb59
its-tusharAg <officialtjtushar@gmail.com>
2024-04-05 00:02:47 +0530

    updating code

---
+++ Learning/Aapna Collage/Delta/31-40/day 33/index.js
@@ -0,0 +1,20 @@
+// const express = require("express");
+// const app = express();
+
+// app.listen(8000, () => {
+//     console.log("Port is listenign at post 8000");
+// });
+
+// app.use(express.urlencoded({extended: true}));
+
+// app.get("/request", (req, res) => {
+//     let {user, pass} = req.query;
+//     res.send(`Send GET response. Welcome ${user}`);
+// })
+// app.post("/request", (req, res) => {
+//     let {user, pass} = req.body;
+//     res.send(`Sending POST response. welcome ${user}!!`);
+// }) 
+const arr = [1,2,3];
+const arr2 = [1,2,3];
+ 
