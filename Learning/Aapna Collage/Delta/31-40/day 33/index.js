a9e5c7cbaef6b190b421718fd3f778fe304b273b
its-tusharAg <officialtjtushar@gmail.com>
2024-04-05 23:05:33 +0530

    a

--- Learning/Aapna Collage/Delta/31-40/day 33/index.js
+++ Learning/Aapna Collage/Delta/31-40/day 33/index.js
@@ -15,6 +15,20 @@
 //     let {user, pass} = req.body;
 //     res.send(`Sending POST response. welcome ${user}!!`);
 // }) 
-const arr = [1,2,3];
-const arr2 = [1,2,3];
+// const arr = [1,2,3];
+// const arr2 = [1,2,3];
  
+
+class Student{
+    constructor(name, age, marks){
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+    talk() {
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+let p1 = new Student("Tushar", 25, 99);
+let p2 = new Student("Rahul", 25, 99);
