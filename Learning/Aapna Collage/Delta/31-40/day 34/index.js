d302d64151164d4f2c5750159958f93acba3356d
its-tusharAg <officialtjtushar@gmail.com>
2024-04-08 20:27:19 +0530

    day 34 almost done

--- Learning/Aapna Collage/Delta/31-40/day 34/index.js
+++ Learning/Aapna Collage/Delta/31-40/day 34/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const {v4: uuidv4} = require("uuid");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -16,14 +17,17 @@ app.listen(8000, () => {
 
 let posts = [
     {
+        id: uuidv4(),
         username: "tusharAg",
         content: "halwa hai dosto",
     },
     {
+        id: uuidv4(),
         username: "bhabkaDevi",
         content: "raat ko hoga hangema, jb chamekega chanda mana...Yo",
     },
     {
+        id: uuidv4(),
         username: "kabirSingh",
         content: "tuu too single hi marega",
     }
@@ -34,11 +38,22 @@ app.get("/posts", (req, res) => {
     res.render("index.ejs", {posts})
 });
 
+// create new post
 app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
 
+// searching by id
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render("show.ejs", {post})
+});
+
+
 app.post("/posts", (req, res) => {
-    console.log(req.body);
-    res.send("Send ho geya");
+    let {username, content} = req.body;
+    let id = uuidv4();
+    posts.push({id, username, content});
+    res.redirect("/posts");
 });
